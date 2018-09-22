d974e690261222e531ecbd5f2b23f986077a4ebb
Eric Abenojar <abenoeric@gmail.com>
2018-09-22 11:44:23 -0500

    Finished some algo problems

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   var arr1 = [];
+//   var mainArr = [];
+//   for (var i = 0; i < array.length; i++) {
+//     if (arr1.length <= size) {
+//       arr1.push(array[i]);
+//       if (arr1.length === size) {
+//         mainArr.push(arr1);
+//         arr1 = [];
+//       } else if (array.length - 1 === i) {
+//         mainArr.push(arr1);
+//       }
+//     }
+//   }
+//   console.log(mainArr);
+//   return mainArr;
+// }
+
+function chunk(array, size) {
+  var index = 0;
+  var chunk = [];
+  var newSize = size;
+  while (index < array.length) {
+    chunk.push(array.slice(index, newSize));
+    index += size;
+    newSize += size;
+  }
+  console.log(chunk);
+  return chunk;
+}
 
 module.exports = chunk;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  var obj = {};
+  var count = 0;
+  var winnerChar = "";
+  var arr = str.split("");
+  arr.forEach(item => {
+    if (!obj[item]) {
+      obj[item] = 1;
+    } else {
+      obj[item]++;
+    }
+  });
+  for (var key in obj) {
+    if (obj[key] > count) {
+      count = obj[key];
+      winnerChar = key;
+    }
+  }
+  return winnerChar;
+}
 
 module.exports = maxChar;
