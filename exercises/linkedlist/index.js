f3d1981a4d1dbeb72b4193bb6bdb5081f9bad2c3
Eric Abenojar <abenoeric@gmail.com>
2018-09-28 22:28:26 -0500

    Solved insertfirst on linkedlist method

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -9,6 +9,13 @@ class Node {
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+}
 
 module.exports = { Node, LinkedList };
